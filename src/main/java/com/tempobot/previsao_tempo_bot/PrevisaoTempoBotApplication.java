5cfe4c6500622dcfb936c52b9ff2893c6912721f
luisf <luisfelipefv8@hotmail.com>
2025-09-08 12:13:49 -0300

    commit inicial

---
+++ src/main/java/com/tempobot/previsao_tempo_bot/PrevisaoTempoBotApplication.java
@@ -0,0 +1,13 @@
+package com.tempobot.previsao_tempo_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrevisaoTempoBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrevisaoTempoBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tempobot/previsao_tempo_bot/PrevisaoTempoBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tempobot.previsao_tempo_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrevisaoTempoBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
